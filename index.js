16213a46904389fd260374f3112a619b3c80a960
karan <karan@agnitotechnologies.com>
2023-11-21 16:20:45 +0530

    authentication

--- index.js
+++ index.js
@@ -5,15 +5,26 @@ const app = express();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const morgan = require("morgan");
+const { jwtDecode } = require("jwt-decode");
 app.use(express.json());
 app.use(morgan());
 mongoose.connect("mongodb://127.0.0.1:27017/test");
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: { type: String, unique: true },
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    password: { type: String, unique: true },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: function (doc, ret) {
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
@@ -33,6 +44,52 @@ const userLoginSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 });
+
+function generateAndSendToken(user, res) {
+  const token = jwt.sign({ userId: user._id }, "your-secret-key", {
+    expiresIn: "1h",
+  });
+  res.json({ token });
+}
+
+const SECRETSTRING =
+  "vbobivnksanvdflbls1313vnvnlnVGVb6s8fgb6s1fg6bs1gf6bsg1s65gfb1sgf";
+
+// Middleware to check for a valid authorization token
+// const authenticateToken = (req, res, next) => {
+//   const token = req.header("Authorization");
+//   console.log(token, "token");
+//   if (!token) {
+//     return res.status(401).send("Unauthorized: Missing token");
+//   }
+
+//   jwt.verify(token, "your-secret-key", (err, user) => {
+//     console.log(err, user);
+//     if (err) {
+//       return res.status(403).send("Forbidden: Invalid token");
+//     }
+
+//     req.user = user;
+//     next();
+//   });
+// };
+
+// Middleware to check for a valid authorization token
+function authenticateToken(req, res, next) {
+  const token1 = req.header("Authorization");
+  const token = token1.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorised" });
+  }
+  try {
+    const decoded = jwt.verify(token, SECRETSTRING);
+    req.user = decoded;
+    next();
+  } catch (ex) {
+    res.status(400).json({ message: "Invalid token." });
+  }
+}
 
 app.post("/add-users", async (req, res) => {
   try {
@@ -55,7 +112,7 @@ app.post("/add-users", async (req, res) => {
   }
 });
 
-app.put("/users/:id", async (req, res) => {
+app.put("/users/:id", authenticateToken, async (req, res) => {
   try {
     const { error, value } = userUpdateSchema.validate(req.body);
 
@@ -74,6 +131,18 @@ app.put("/users/:id", async (req, res) => {
     }
 
     res.send(updatedUser);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.deleteOne({ _id: req.params.id });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.status(400).send({ message: "Deleted Successfully", data: user });
   } catch (error) {
     res.status(400).send({ error: error.message });
   }
@@ -118,10 +187,13 @@ app.post("/login", async (req, res) => {
         .status(401)
         .send({ message: "Invalid email or password 3333" });
     }
-    const token = jwt.sign({ userId: user._id }, "your-secret-key", {
+    const token = jwt.sign({ userId: user._id }, SECRETSTRING, {
       expiresIn: "1h",
     });
-    res.json({ token });
+
+    const decoded = jwtDecode(token);
+
+    res.json({ token, decoded });
   } catch (error) {
     res.status(400).send({ error: error.message });
   }
